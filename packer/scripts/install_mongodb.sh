e4df57ce3c7f390690fb5600e5b5ef864f6d4840
russo1982 <rusyv1982@yandex.ru>
2023-04-30 22:26:05 +0300

    ruby and monodb scripts edited

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,44 +1,9 @@
 #!/bin/bash
 
-# --------------------------------------------------------------------------------------------
-# Current script installs MongoDB ( mongodb-org )
-#
-# Special thanks to:
-#   blacktm script, found here:
-#   https://gist.github.com/blacktm/8302741
-# --------------------------------------------------------------------------------------------
-
-
-# Welcome message
-echo "This will install MongoDB on the current machine."
-
-# Prompt to continue
-#read -p "  Continue? (y/n) " ans
-#if [[ $ans != "y" ]]; then
-#  echo -e "\nQuitting...\n"
-#  exit
-#fi
-#echo
-
-# Time the install process
-START_TIME=$SECONDS
-
-# Add GPG key and MongoDB repo
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-
-# update the list of packages available to install
-sudo apt update
-
-# install MongoDB
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
-sudo systemctl status mongod
-
-# Print the time elapsed
-ELAPSED_TIME=$(($SECONDS - $START_TIME))
-echo -e "\nFinished in $(($ELAPSED_TIME/60/60)) hr, $(($ELAPSED_TIME/60%60)) min, and $(($ELAPSED_TIME%60)) sec\n"
-echo "------------------------------------------------"
-echo "Installed MongoDB version is:  $(mongod --version)"
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add - # После пайпа sudo не убрано - а то не отработает добавление ключа
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
 
