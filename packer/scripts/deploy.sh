7be1b8643a3e7c05b9c16a3602be76432a05284e
russo1982 <rusyv1982@yandex.ru>
2023-05-01 18:19:05 +0300

    reddit-full image settings done. Test was succesful. Image created

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -1,36 +1,19 @@
 #!/bin/bash
 
-# --------------------------------------------------------------------------------------------
-# Current script a software from https://github.com/express42/reddit.git 
-#
-#Special thanks to:
-#   blacktm script, found here:
-#   https://gist.github.com/blacktm/8302741
-# --------------------------------------------------------------------------------------------
+sudo echo '[Unit]' > /tmp/puma.service
+sudo echo 'Description=Puma' >> /tmp/puma.service
+sudo echo 'After=network.target' >> /tmp/puma.service
+sudo echo '[Service]' >> /tmp/puma.service
+sudo echo 'Type=simple' >> /tmp/puma.service
+sudo echo 'WorkingDirectory=$HOME/reddit' >> /tmp/puma.service
+sudo echo 'ExecStart=/usr/local/bin/puma' >> /tmp/puma.service
+sudo echo 'Restart=on-failure' >> /tmp/puma.service
+sudo echo '[Install]' >> /tmp/puma.service
+sudo echo 'WantedBy=multi-user.target' >> /tmp/puma.service
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+cd $HOME/reddit
 
-
-# Welcome message
-echo "This will deploy a software from https://github.com/express42/reddit.git"
-echo
-# Prompt to continue
-#read -p "  Continue? (y/n) " ans
-#if [[ $ans != "y" ]]; then
-#  echo -e "\nQuitting...\n"
-#  exit
-#fi
-#echo
-
-# Time the install process
-START_TIME=$SECONDS
-
-# Check if git installed
-GIT_INSTALLED=$(which git)
-
-if [[ $GIT_INSTALLED == "" ]]; then
-  echo " ----- ----- ---->> git is missing and will be installed"
-  echo
-  sudo apt-get install -y git # installing git
-fi
+sudo apt-get install -y git # installing git
 
 # clone git repo of reddit
 cd $HOME
@@ -40,14 +23,4 @@ git clone -b monolith https://github.com/express42/reddit.git
 cd $HOME/reddit && bundle install
 
 # Start puma deamon
-puma -d
-
-
-# Print the time elapsed
-ELAPSED_TIME=$(($SECONDS - $START_TIME))
-echo -e "\nFinished in $(($ELAPSED_TIME/60/60)) hr, $(($ELAPSED_TIME/60%60)) min, and $(($ELAPSED_TIME%60)) sec\n"
-echo "------------------------------------------------"
-ps aux | grep puma
-echo
-echo "Software is active"
-
+sudo systemctl daemon-reload && sudo systemctl start puma && sudo systemctl enable puma
