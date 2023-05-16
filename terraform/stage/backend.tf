ea87e0d9fd59df9f2e254794020c97a0c36e3559
russo1982 <rusyv1982@yandex.ru>
2023-05-16 14:54:23 +0300

    json files corrected

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -7,12 +7,10 @@ terraform {
     bucket     = "terr-state-file"
     region     = "ru-central1"
     key        = "stage/terraform.tfstate"
-    access_key = "YCAJE7V_ryhfk-9EKPyrG1mVi"
-    secret_key = "YCOx3MIZOU48_GD3wC1JwSTaP_xI-8i_HcuTNXHD"
+    access_key = "YCAJEPyrG1mVi"
+    secret_key = "YCOx3MIZOU4aP_xI-8i_HcuTNXHD"
 
     skip_region_validation      = true
     skip_credentials_validation = true
   }
 }
-
-
