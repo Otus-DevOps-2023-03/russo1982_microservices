770499de41b8d5eab4f2da273b07e109be073852
russo1982 <rusyv1982@yandex.ru>
2023-06-05 22:44:55 +0300

    File .travis.yml added/ Not sure gona work

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -7,8 +7,8 @@ terraform {
     bucket     = "terr-state-file"
     region     = "ru-central1"
     key        = "stage/terraform.tfstate"
-    access_key = "YCAJE7V_ryhfk-9EKPyrG1mVi"
-    secret_key = "YCOx3MIZOU48_GD3wC1JwSTaP_xI-8i_HcuTNXHD"
+    access_key = "YCAJE"
+    secret_key = "YCOx3MI"
 
     skip_region_validation      = true
     skip_credentials_validation = true
